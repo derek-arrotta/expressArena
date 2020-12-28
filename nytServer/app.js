ff51e9a1f93a75bdbe1aa6a789b197c711d61c07
Derek Arrotta <derek.arrotta@gmail.com>
2020-12-28 15:29:55 -0500

    udpate

--- nytServer/app.js
+++ nytServer/app.js
@@ -1,12 +1,9 @@
 const express = require('express');
 const morgan = require('morgan');
+const app = express();
+app.use(morgan('common')); // let's see what 'common' format looks like
 const cors = require('cors');
 app.use(cors());
-
-const app = express();
-
-app.use(morgan('common')); // let's see what 'common' format looks like
-
 const books = require('./books-data.js');
 
 app.get('/books', (req, res) => {
